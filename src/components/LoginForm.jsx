73a9183423710debe849ed4cf818d52593b9ec63
Matheus Araripe <matheus.alc2001@gmail.com>
2023-05-31 17:51:27 -0300

    feat: componente de login criado e adição da lib yup

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+// React hook form
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+
+// Yup
+import * as yup from "yup";
+
+const schema = yup.object({
+    email: yup.string().required("Campo obrigaório"),
+    password: yup.string().required("Campo obrigaório")
+})
+
+
+const LoginForm = () => {
+
+  const { register, handleSubmit: onSubmit, formState: { errors } } = useForm({resolver: yupResolver(schema)});
+  const handleSubmit = (data) => console.log(data);
+
+  return (
+    <div className="login-form">
+      <h2>Entrar</h2>
+      <form onSubmit={onSubmit(handleSubmit)}>
+        <div className="form-group">
+          <label htmlFor='email'>Email</label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            {...register("email")}
+          />
+          <span className="error">
+            {errors.email?.message}
+          </span>
+        </div>
+        <div className="form-group">
+          <label htmlFor='password'>Password</label>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            {...register("password")}
+          />
+          <span className="error">
+            {errors.email?.message}
+          </span>
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
