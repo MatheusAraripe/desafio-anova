1dea618547ed907ccf7b1ce27300c3cb67a3feda
Matheus Araripe <matheus.alc2001@gmail.com>
2023-06-01 16:33:40 -0300

    fix: todas as funcões de autenticação agora estão no arquivo auth.jsx

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 
 import { 
     BrowserRouter as Router,
@@ -11,31 +11,19 @@ import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 
 // Importe do context
-import { AuthContext } from "./contexts/auth";
+import { AuthProvider } from "./contexts/auth";
 
 
 const AppRoutes = () => {
 
-    const [user, setUser] = useState(null);
-
-    const login = (email, password) => {
-        console.log("login", {email, password});
-        setUser({id: '1', email});
-    };
-
-    const logout = () => {
-        console.log("logout");
-    };
-
-
     return(
         <Router>
-            <AuthContext.Provider value={{authenticated: !!user, user, login, logout}}>
+            <AuthProvider>
                 <Routes>
                     <Route exact path="/login" element={<LoginPage />} />
                     <Route exact path="/" element={<HomePage />} />
                 </Routes>
-            </AuthContext.Provider>
+            </AuthProvider>
         </Router>
     )
 }
--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -1,3 +1,23 @@
-import {createContext} from "react";
+import {createContext, useState} from "react";
 
 export const AuthContext = createContext();
+
+export const AuthProvider = ({children}) => {
+
+    const [user, setUser] = useState(null);
+
+    const login = (email, password) => {
+        console.log("login", {email, password});
+        setUser({id: '1', email});
+    };
+
+    const logout = () => {
+        console.log("logout");
+    };
+
+    return(
+        <AuthContext.Provider value={{authenticated: !!user, user, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
