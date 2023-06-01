26c181e1e8ca427ee8fe24a09bf68c8ce6763293
Matheus Araripe <matheus.alc2001@gmail.com>
2023-06-01 17:50:22 -0300

    feat: utilização do local storage para armazenar usuários

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -16,9 +16,14 @@ import { AuthContext, AuthProvider } from "./contexts/auth";
 
 const AppRoutes = () => {
 
-    const Private = ({children}) => {
-        const {authenticated} = useContext(AuthContext)
 
+    // criação da área privada(apenas usuários logados)
+    const Private = ({children}) => {
+        const {authenticated, loading} = useContext(AuthContext)
+
+        if (loading) {
+            return <div className="loading"> Carregando...</div>;
+        }
         if(!authenticated){
             return <Navigate to="/login" />
         }
--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -1,4 +1,4 @@
-import {createContext, useState} from "react";
+import {createContext, useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
@@ -7,11 +7,36 @@ export const AuthProvider = ({children}) => {
 
     const navigate = useNavigate();
 
+    // atribui se o usuário está logado ou não
     const [user, setUser] = useState(null);
 
+    // loading para esperar a aplicação recuperar user no local storage
+    const [loading, setLoading] = useState(true);
+
+
+    // useEffect checa toda vez que a pagina é recarregada se o usuário
+    // ainda está logado ou não
+    useEffect( () => {
+        const recoveredUser = localStorage.getItem("user");
+
+        if(recoveredUser){
+            setUser(JSON.parse(recoveredUser));
+        }
+
+        setLoading(false);
+    },[]);
+
     const login = (email, password) => {
+
+        const loggedUser = {
+            id: "1",
+            email
+        }
+
+        localStorage.setItem("user", JSON.stringify(loggedUser));
+
         if(password === "12345"){
-            setUser({id: '1', email});
+            setUser(loggedUser);
             navigate("/");
         }
     };
@@ -22,7 +47,7 @@ export const AuthProvider = ({children}) => {
     };
 
     return(
-        <AuthContext.Provider value={{authenticated: !!user, user, login, logout}}>
+        <AuthContext.Provider value={{authenticated: !!user, user, loading, login, logout}}>
             {children}
         </AuthContext.Provider>
     );
