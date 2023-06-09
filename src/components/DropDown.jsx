4fcce70214226827e7e47a60c4c4c455a536c52b
Matheus Araripe <matheus.alc2001@gmail.com>
2023-06-09 13:56:08 -0300

    feat: adicionando o componente de dropdown na navbar

---
+++ src/components/DropDown.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function DropDown() {
+  return (
+    <div>DropDown</div>
+  )
+}
+
+export default DropDown
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,22 @@
-import React, {useContext} from 'react'
+// eslint-disable-next-line no-unused-vars
+import React, {useContext, useState} from 'react'
+import {IoMdArrowDropdown} from "react-icons/io";
 import { AuthContext } from '../contexts/auth';
+import { Link } from 'react-router-dom';
+import DropDown from './DropDown';
+
+
 function NavBar() {
+
+  const [dropdown, setDropdown] = useState(false)
+
+  const onMouseEnter = () => {
+    setDropdown(true)
+  }
+
+  const onMouseLeave = () => {
+      setDropdown(false)
+  }
 
   const {logout} = useContext(AuthContext);
   const handleLogout = () => {
@@ -12,6 +28,10 @@ function NavBar() {
         <h3 className="nav-logo">
             Telefones uteis
         </h3>
+        <Link onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+          <IoMdArrowDropdown />
+          {dropdown && <DropDown />}
+        </Link>
         <button onClick={handleLogout}>Logout</button>
     </div>
   )
