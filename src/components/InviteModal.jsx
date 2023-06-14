a04d822e87c04fc1d676a147e4b9ff9c91ded5ea
Matheus Araripe <matheus.alc2001@gmail.com>
2023-06-14 17:22:31 -0300

    feat: modal de convidar colaborador adicionado

--- src/components/InviteModal.jsx
+++ src/components/InviteModal.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
+import {BsEnvelopePaperHeartFill} from "react-icons/bs"
 
 function InviteModal({setOpenInviteModal}) {
   return (
-    <div>
-        <h2>Convidar</h2>
-        <button onClick={ ()=> setOpenInviteModal(false)}>cancelar</button>
+    <div className='modal-background'>
+        <div className='invite-modal-container'>
+            <div className="invite-modal-body">
+                <div className="invite-text-container">
+                    <button onClick={ ()=> setOpenInviteModal(false)}>X</button>
+                    <div className="invite-icon-container">
+                        <BsEnvelopePaperHeartFill className='invite-icon'/>
+                    </div>
+                    <h3 className='invite-modal-title'>Convide um colaborador</h3>
+                    <p className='invite-modal-text'>Informe o email de um colaborador para que ele receba um convite de cadastro</p>
+                </div>
+                <div className="invite-email-container">
+                    <input type="email" className='invite-email'/>
+                </div>
+            </div>
+            <div className="footer-invite">
+                <button className='invite-user-btn'>ENVIAR CONVITE</button>
+            </div>
+        </div>
     </div>
+
   )
 }
 
-export default InviteModal
+export default InviteModal;
