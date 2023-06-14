007194c8ba4cef0554ad7faeec74e8e0d2730143
Matheus Araripe <matheus.alc2001@gmail.com>
2023-06-14 14:03:38 -0300

    feat: adição dos inputs no modal

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -6,7 +6,17 @@ function AddModal({setOpenModal}) {
     <div className='modal-background'>
         <div className="modal-container">
             <div className="body">
-                <p>Modal body</p>
+                <div className="input-name">
+                  <input type="text" className="user-name" name='name'/>
+                </div>
+                <div className="input-informations">
+                  <input type="text" />
+                  <input type="number" />
+                  <input type="email" name='email' />
+                </div>
+                <div className="input-tel">
+                  <input type="tel" />
+                </div>
             </div>
             <div className="footer">
                 <button className='close-modal-btn' onClick={ ()=> setOpenModal(false)}>Cancelar</button>
