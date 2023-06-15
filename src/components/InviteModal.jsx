79c39684066397862a1f43c90d37d3ffb8aa9498
Matheus Araripe <matheus.alc2001@gmail.com>
2023-06-15 11:39:50 -0300

    feat: validação de convite de usuário

--- src/components/InviteModal.jsx
+++ src/components/InviteModal.jsx
@@ -1,26 +1,48 @@
 import React from 'react'
 import {BsEnvelopePaperHeartFill} from "react-icons/bs"
 
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+
+
+import * as yup from "yup";
+
+const schema = yup.object({
+    email: yup.string().required("Campo obrigaório")
+})
+
 function InviteModal({setOpenInviteModal}) {
+
+    const { register, handleSubmit: onSubmit, formState: { errors } } = useForm({resolver: yupResolver(schema)});
+    const handleSubmit = (data) => {
+      console.log(data);
+    };
+
+
   return (
     <div className='modal-background'>
         <div className='invite-modal-container'>
-            <div className="invite-modal-body">
-                <div className="invite-text-container">
-                    <button onClick={ ()=> setOpenInviteModal(false)}>X</button>
-                    <div className="invite-icon-container">
-                        <BsEnvelopePaperHeartFill className='invite-icon'/>
+            <form onSubmit={onSubmit(handleSubmit)}>
+                <div className="invite-modal-body">
+                    <div className="invite-text-container">
+                        <button onClick={ ()=> setOpenInviteModal(false)}>X</button>
+                        <div className="invite-icon-container">
+                            <BsEnvelopePaperHeartFill className='invite-icon'/>
+                        </div>
+                        <h3 className='invite-modal-title'>Convide um colaborador</h3>
+                        <p className='invite-modal-text'>Informe o email de um colaborador para que ele receba um convite de cadastro</p>
                     </div>
-                    <h3 className='invite-modal-title'>Convide um colaborador</h3>
-                    <p className='invite-modal-text'>Informe o email de um colaborador para que ele receba um convite de cadastro</p>
+                    <div className="invite-email-container">
+                        <input type="email" name='email' className='invite-email' {...register("email")}/>
+                    </div>
+                    <span className="error">
+                        {errors.email?.message}
+                    </span>
                 </div>
-                <div className="invite-email-container">
-                    <input type="email" className='invite-email'/>
+                <div className="footer-invite">
+                    <button className='invite-user-btn'>ENVIAR CONVITE</button>
                 </div>
-            </div>
-            <div className="footer-invite">
-                <button className='invite-user-btn'>ENVIAR CONVITE</button>
-            </div>
+            </form>
         </div>
     </div>
 
