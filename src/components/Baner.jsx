0e5db69c702c1428b5b4a588b9551cd340fa9f4d
Matheus Araripe <matheus.alc2001@gmail.com>
2023-06-16 14:38:08 -0300

    fix: removendo o componente da lib react-select

--- src/components/Baner.jsx
+++ src/components/Baner.jsx
@@ -1,19 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import "../styles/baner.scss"
-// import Select from 'react-select'
-
-const options = [
-  { value: 'Rio de Janeiro', label: 'Rio de Janeiro' },
-  { value: 'Juiz de Fora', label: 'Juiz de Fora' },
-  { value: 'Sao Paulo', label: 'Sao Paulo' }
-]
 
 function Baner() {
   return (
     <div className='baner-container'>
-      <Select options={options} className='unidade-input'/>
-      <input type="text" name="" id="" className='nome-input'/>
+      <input type="text" className="input unidade-input" />
+      <input type="text" name="" id="" className='input nome-input'/>
     </div>
   )
 }
