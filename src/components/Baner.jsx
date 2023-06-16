58eb7aa806571a9272ea541017f8b6974ec1b72b
Matheus Araripe <matheus.alc2001@gmail.com>
2023-06-16 12:10:39 -0300

    feat: adicionando os imports para react-select
    BREAKING CHANGE: biblioteaca nao installada

--- src/components/Baner.jsx
+++ src/components/Baner.jsx
@@ -1,11 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import "../styles/baner.scss"
+// import Select from 'react-select'
+
+const options = [
+  { value: 'Rio de Janeiro', label: 'Rio de Janeiro' },
+  { value: 'Juiz de Fora', label: 'Juiz de Fora' },
+  { value: 'Sao Paulo', label: 'Sao Paulo' }
+]
 
 function Baner() {
   return (
     <div className='baner-container'>
-      <input type="text" className='unidade-input'/>
+      <Select options={options} className='unidade-input' />
       <input type="text" name="" id="" className='nome-input'/>
     </div>
   )
