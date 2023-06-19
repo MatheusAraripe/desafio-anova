7a487d981b6b7db9b79fc42ecf13cec6bf0e574b
Matheus Araripe <matheus.alc2001@gmail.com>
2023-06-19 13:05:32 -0300

    feat: adição do botão de busca no baner

--- src/components/Baner.jsx
+++ src/components/Baner.jsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react'
+import {BiSearchAlt} from "react-icons/bi"
 import Select from './Select'
 import "../styles/baner.scss"
 
@@ -10,6 +11,12 @@ function Baner() {
     <div className='baner-container'>
       <Select selected={selected} setSelected={setSelected}/>
       <input type="text" name="" id="" className='nome-input' placeholder='Digite o nome do colaborador'/>
+      {/* <button type='submit' className="search-icon-container">
+       <BiSearchAlt className='search-icon'/>
+      </button> */}
+      <div className="search-icon-container">
+        <BiSearchAlt className='search-icon'/>
+      </div>
     </div>
   )
 }
