9bc73386a035ae106d0edd8151d199c364c36ede
Matheus Araripe <matheus.alc2001@gmail.com>
2023-06-20 16:44:09 -0300

    feat: adição de css ao  modal de excluir card

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import "../styles/delete_modal.scss"
 import {MdDeleteForever} from "react-icons/md"
 
 function DeleteModal(setOpenDeleteModal) {
   return (
     <div className='modal-background'>
-      <div className="invite-modal-container">
+      <div className="delete-modal-container">
         <div className='invite-modal-body'>
           <div className="invite-text-container">
             <div className="invite-icon-container">
@@ -12,6 +13,10 @@ function DeleteModal(setOpenDeleteModal) {
             </div>
             <h3 className='invite-modal-title'>Excluir card?</h3>
             <p className='invite-modal-text'>Essa ação não poderá ser desfeita</p>
+          </div>
+          <div className="footer-invite">
+            <button className='cancel-op-btn'>CANCELAR</button>
+            <button className='excluir-btn'>EXCLUIR</button>
           </div>
         </div>
       </div>
