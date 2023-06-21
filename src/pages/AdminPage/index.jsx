530197586528583b56f02fe4e001ee1479f48269
Matheus Araripe <matheus.alc2001@gmail.com>
2023-06-21 12:05:08 -0300

    faet: adição da navbar e titulo a pagina de admin

--- src/pages/AdminPage/index.jsx
+++ src/pages/AdminPage/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import NavBar from '../../components/NavBar'
 
 function AdminPage() {
   return (
-    <div>AdminPage</div>
+    <div className='admin-page-container'>
+        <NavBar />
+        <div className="admin-title-container">
+            <h2 className='admin-title'>Gerenciar Usuários</h2>
+        </div>
+    </div>
   )
 }
 
