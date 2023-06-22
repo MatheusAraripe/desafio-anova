59e2947829dbb155a7f337cfe98a8bd73c2f4539
Matheus Araripe <matheus.alc2001@gmail.com>
2023-06-22 13:23:07 -0300

    feat: adição de estilo ao cabesalho da tabela

--- src/components/TableAdmin.jsx
+++ src/components/TableAdmin.jsx
@@ -3,20 +3,20 @@ import {FaPencilAlt, FaTrashAlt} from "react-icons/fa"
 
 const TableAdmin = () => {
   const contatos = [
-    { nome: 'João', unidade: 'Rio de Janeiro', ramal: '1234', telefone: '(11) 1234-5678', email: 'joao@ntendencia.com' },
-    { nome: 'Maria', unidade: 'Juiz de Fora', ramal: '5678', telefone: '(22) 5678-9012', email: 'maria@ntendencia.com' },
-    { nome: 'Pedro', unidade: 'São Paulo', ramal: '9012', telefone: '(33) 9012-3456', email: 'pedro@ntendencia.com' }
+    { nome: 'João', unidade: 'Rio de Janeiro', ramal: '1234', telefones: '(11) 1234-5678', email: 'joao@ntendencia.com' },
+    { nome: 'Maria', unidade: 'Juiz de Fora', ramal: '5678', telefones: '(22) 5678-9012', email: 'maria@ntendencia.com' },
+    { nome: 'Pedro', unidade: 'São Paulo', ramal: '9012', telefones: '(33) 9012-3456', email: 'pedro@ntendencia.com' }
   ];
 
   return (
     <table >
       <thead>
         <tr>
-          <th>Nome</th>
+          <th className='th-name'>Nome</th>
           <th>Unidade</th>
-          <th>Ramal</th>
-          <th>Telefone</th>
-          <th>Email</th>
+          <th className='th-ramal'>Ramal</th>
+          <th>Telefones</th>
+          <th className='th-email'>Email</th>
         </tr>
       </thead>
       <tbody>
@@ -25,7 +25,7 @@ const TableAdmin = () => {
             <td>{contato.nome}</td>
             <td>{contato.unidade}</td>
             <td>{contato.ramal}</td>
-            <td>{contato.telefone}</td>
+            <td>{contato.telefones}</td>
             <td>{contato.email}</td>
             <td><FaPencilAlt /></td>
             <td><FaTrashAlt /></td>
